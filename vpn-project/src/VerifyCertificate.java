5306b041fd7cf5a42244e79f9bfb54958bed7d92
Unknown <julius.cc@hotmail.com>
2018-12-02 14:53:57 +0100

    Fixed task 3

---
+++ vpn-project/src/VerifyCertificate.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+public class VerifyCertificate {
+    public static void main(String[] args) throws IOException, CertificateException {
+        if (args.length < 2) {
+            System.exit(-1);
+        }
+
+        String caFilePath = args[0];
+        String userFilePath = args[1];
+
+        X509Certificate caCert = getCertificate(caFilePath);
+        X509Certificate userCert = getCertificate(userFilePath);
+
+        System.out.println(caCert.getSubjectDN());
+        System.out.println(userCert.getSubjectDN());
+
+        try {
+            caCert.verify(caCert.getPublicKey());
+            userCert.verify(caCert.getPublicKey());
+            System.out.println("Pass");
+        } catch (Exception e) {
+            System.out.println("Fail");
+        }
+    }
+
+
+    /**
+     * Copied from: https://docs.oracle.com/javase/7/docs/api/java/security/cert/X509Certificate.html
+     */
+    private static X509Certificate getCertificate(String caFilePath) throws IOException, CertificateException {
+        X509Certificate cert;
+        InputStream inStream = null;
+        try {
+            inStream = new FileInputStream(caFilePath);
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            cert = (X509Certificate) cf.generateCertificate(inStream);
+        } finally {
+            if (inStream != null) {
+                inStream.close();
+            }
+        }
+        return cert;
+    }
+
+}
+
