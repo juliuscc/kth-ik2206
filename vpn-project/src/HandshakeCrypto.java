16632555ee29a68085793363ac9fcf5ebc5c64d9
Unknown <julius.cc@hotmail.com>
2018-12-07 09:23:42 +0100

    Made HandshakeCrypto independent

--- vpn-project/src/HandshakeCrypto.java
+++ vpn-project/src/HandshakeCrypto.java
@@ -2,12 +2,15 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
@@ -29,7 +32,7 @@ public class HandshakeCrypto {
     }
 
     public static PublicKey getPublicKeyFromCertFile(String certfile) throws IOException, CertificateException {
-        X509Certificate certificate = VerifyCertificate.getCertificate(certfile);
+        X509Certificate certificate = getCertificate(certfile);
         return certificate.getPublicKey();
     }
 
@@ -42,5 +45,23 @@ public class HandshakeCrypto {
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 
         return keyFactory.generatePrivate(keySpec);
+    }
+
+    /**
+     * Copied from: https://docs.oracle.com/javase/7/docs/api/java/security/cert/X509Certificate.html
+     */
+    public static X509Certificate getCertificate(String certificateFilePath) throws IOException, CertificateException {
+        X509Certificate cert;
+        InputStream inStream = null;
+        try {
+            inStream = new FileInputStream(certificateFilePath);
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            cert = (X509Certificate) cf.generateCertificate(inStream);
+        } finally {
+            if (inStream != null) {
+                inStream.close();
+            }
+        }
+        return cert;
     }
 }
